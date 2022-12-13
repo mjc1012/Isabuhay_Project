3974536d5e177a19ab25c37312c3d7f798a965df
Marc John <corralmarcjohno.com>
2022-12-13 12:04:55 +0800

    changes

--- IsabuhayWebsite/settings.py
+++ IsabuhayWebsite/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure--c#2qwydxaizl1x)0--ujwz+winjsqwqn7ctyq1ek5^gwn7@d3
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = ['project-isabuhay.herokuapp.com', 'web-production-f153.up.railway.app', 'https://web-production-f153.up.railway.app']
+ALLOWED_HOSTS = ['project-isabuhay.herokuapp.com', 'web-production-f153.up.railway.app', 'https://web-production-f153.up.railway.app']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
